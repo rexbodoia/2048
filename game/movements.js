629d7ec266a24aded3ffe4f45ef0eb8ca4a3e0b7
Rex Bodoia <rex@bodoia.com>
2018-08-23 17:05:59 -0700

    Implement collision aware motion in all directions but without merges

--- game/movements.js
+++ game/movements.js
@@ -28,10 +28,66 @@ Board.prototype.update = function(dir) {
             tile = this.grid[row][col - 1];
             this.grid[row][col] = null;
             col -= 1;
-            console.log(this);
           }
           if (col > 0 && tile.number == this.grid[row][col - 1].number) {
+            merge(tile, this.grid[row][col - 1]);
+         }
+        }
+      }
+    }
+    break;
+    case 'right':
+    for (let row = 0; row < board.size; row++) {
+      for (let col = board.size - 2; col >= 0; col--) {
+        let tile = this.grid[row][col];
+        if (tile != null) {
+          while(col < board.size - 1 && this.grid[row][col + 1] == null) {
+            tile.moveOne(row, col, board, dir);
+            this.grid[row][col + 1] = tile;
+            tile = this.grid[row][col + 1];
+            this.grid[row][col] = null;
+            col += 1;
+          }
+          if (col < board.size - 1 && tile.number == this.grid[row][col + 1].number) {
+            merge(tile, this.grid[row][col + 1]);
+         }
+        }
+      }
+    }
+    break;
+    case 'up':
+    for (let col = 0; col < board.size; col++) {
+      for (let row = 1; row < board.size; row++) {
+        let tile = this.grid[row][col];
+        if (tile != null) {
+          while(row > 0 && this.grid[row - 1][col] == null) {
+            tile.moveOne(row, col, board, dir);
+            this.grid[row - 1][col] = tile;
+            tile = this.grid[row - 1][col];
+            this.grid[row][col] = null;
+            row -= 1;
+          }
+          if (row > 0 && tile.number == this.grid[row - 1][col].number) {
             merge(tile, this.grid[row - 1][col]);
+         }
+        }
+      }
+    }
+    break;
+    case 'down':
+      for (let col = 0; col < board.size; col++) {
+        for (let row = board.size - 2; row >= 0; row--) {
+        let tile = this.grid[row][col];
+        if (tile != null) {
+          while(row < board.size - 1 && this.grid[row + 1][col] == null) {
+            tile.moveOne(row, col, board, dir);
+            this.grid[row + 1][col] = tile;
+            tile = this.grid[row + 1][col];
+            this.grid[row][col] = null;
+            row += 1;
+          }
+          if (row < board.size - 1 && tile.number == this.grid[row + 1][col].number) {
+            merge(tile, this.grid[row + 1][col]);
          }
         }
       }
@@ -50,6 +106,42 @@ NumberTile.prototype.moveOne = function(row, col, board, dir) {
         this.number.setAttribute('x', parseInt(this.number.getAttribute('x')) - 4);
         this.left -= 4;
         this.right -= 4;
+        counter += 1;
+        if (counter === board.tileSize / 4) {
+          clearInterval(intr);
+        }
+      }, 1);
+      break;
+    case 'right':
+      intr = setInterval(() => {
+        this.tile.setAttribute('x', parseInt(this.tile.getAttribute('x')) + 4);
+        this.number.setAttribute('x', parseInt(this.number.getAttribute('x')) + 4);
+        this.left += 4;
+        this.right += 4;
+        counter += 1;
+        if (counter === board.tileSize / 4) {
+          clearInterval(intr);
+        }
+      }, 1);
+      break;
+    case 'up':
+      intr = setInterval(() => {
+        this.tile.setAttribute('y', parseInt(this.tile.getAttribute('y')) - 4);
+        this.number.setAttribute('y', parseInt(this.number.getAttribute('y')) - 4);
+        this.top -= 4;
+        this.bottom -= 4;
+        counter += 1;
+        if (counter === board.tileSize / 4) {
+          clearInterval(intr);
+        }
+      }, 1);
+      break;
+    case 'down':
+      intr = setInterval(() => {
+        this.tile.setAttribute('y', parseInt(this.tile.getAttribute('y')) + 4);
+        this.number.setAttribute('y', parseInt(this.number.getAttribute('y')) + 4);
+        this.top += 4;
+        this.bottom += 4;
         counter += 1;
         if (counter === board.tileSize / 4) {
           clearInterval(intr);
